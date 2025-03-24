fd15b15161f44c4dbae367d459501c95830280f0
André Luis de Aguiar Lopes <andrelopes.al@gmail.com>
2025-03-24 16:41:06 -0300

    feat: add lib-minimal

---
+++ include/utils/calculator.h
@@ -0,0 +1,8 @@
+#ifndef CALCULATOR_H_
+#define CALCULATOR_H_
+
+int sum(int a, int b);
+
+int multiply(int a, int b);
+
+#endif
---
+++ src/utils/calculator.c
@@ -0,0 +1,9 @@
+#include "utils/calculator.h"
+
+int sum(int a, int b) {
+  return a + b;
+}
+
+int multiply(int a, int b) {
+  return a * b;
+}
